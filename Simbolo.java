2ac2f3357d104c3463fce56cda853de2e292c3eb
YailPalominos <yail.palominos@gmail.com>
2023-12-04 14:52:16 -0600

    [ADD] Basico

---
+++ Simbolo.java
@@ -0,0 +1,21 @@
+public class Simbolo {
+
+    public String nombre;
+    public String valor;
+    public int repeticion;
+
+    public String token;
+    public String tipo;
+    public int idToken;
+    public String linea;
+
+    public Simbolo() {
+        nombre = "";
+        valor = "";
+        repeticion = 0;
+        token = "";
+        tipo = "";
+        idToken = 0;
+        linea = "";
+    }
+}
